1195a38b9227ea47952f2050b8f60c3cddbd8eb7
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-11 10:39:28 +0100

    Add files via upload

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+   // Only change code below this line
+
+   /* komentar prvi
+   drugi komentar
+   komentar treci */
+
+   // komentar za jednu liniju
+
+   
+   // Only chance code above this line
+
+
+} 
+
+console.log(main()); 
+
+module.exports = main; 
+
