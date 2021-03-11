8831b19d2800e0b329ec231f570acf6fc513e66e
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-11 10:42:41 +0100

    Add files via upload

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+  var sum = numOne + numTwo;
+   
+  return sum;
+    // Only chance code above this line
+
+    
+   
+ } 
+ 
+ console.log(main(15,8)); 
+ console.log(main(5,28)); 
+ console.log(main(150,425));
+ 
+ module.exports = main; 
+ 
