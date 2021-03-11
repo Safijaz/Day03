1c9ae3fb4064b3d0c632ee2572f3cd53e957f3e1
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-11 10:39:56 +0100

    Add files via upload

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+  var forCat;
+
+    // Only chance code above this line
+ 
+ 
+ } 
+ 
+ console.log(main()); 
+ 
+ module.exports = main; 
+ 
+ 
