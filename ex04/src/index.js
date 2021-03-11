dc567b4b01dfa805aede8cb83f4b5aed9fa239ea
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-11 10:40:40 +0100

    Add files via upload

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    // Only change code below this line
+  
+   var x = 10;
+   var y = 10;
+
+   x = y;
+  
+
+   return [x, y];
+
+  
+    // Only chance code above this line
+   
+ } 
+ 
+ console.log(main()); 
+ 
+ module.exports = main; 
