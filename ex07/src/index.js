97dc81cee7c134a493f360f5496ac5ab4736b54c
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-11 10:42:10 +0100

    Add files via upload

---
+++ ex07/src/index.js
@@ -0,0 +1,34 @@
+function main() {
+    // Only change code below this line
+
+    // Variable declaration
+
+  var studlyCapVar;
+  var properCamelCase;
+  var  titleCaseOver;
+
+  // Variable assignment
+
+  var studlyCapVar = 10;
+  var properCamelCase ="A String";
+  var  titleCaseOver = 9000;
+
+
+return  {
+      studlyCapVar,
+      properCamelCase,
+      titleCaseOver};
+
+    // Only chance code above this line
+  
+    
+
+ } 
+ 
+ console.log(main()); 
+ 
+ module.exports = main; 
+ 
+ 
+
+
